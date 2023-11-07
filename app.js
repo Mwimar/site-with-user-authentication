4e1340f74669a4e35c859d5c32cf62b6686bd2f0
Mark Mwima <markmwimar@gmail.com>
2023-11-07 22:25:17 +0300

    creating sessions

--- app.js
+++ app.js
@@ -2,11 +2,23 @@ const path = require("path");
 
 const express = require("express");
 
+const session = require("express-session");
+
+const mongodbStore = require("connect-mongodb-session");
+
 const siteRoutes = require("./routes/site");
 
 const db = require("./data/database");
 
+const MongoDBStore = mongodbStore(session);
+
 const app = express();
+
+const sessionStore = new MongoDBStore({
+  uri: "mongodb://localhost:27017",
+  databaseName: "auth-db",
+  collection: "sessions",
+});
 app.use(express.json());
 
 app.set("view engine", "ejs");
@@ -14,6 +26,15 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true })); //parsing incoming request bodies
 app.use(express.json());
+
+app.use(
+  session({
+    secret: "super-secret",
+    resave: false,
+    saveUnitialized: false,
+    store: sessionStore,
+  })
+);
 
 app.use(express.static("public"));
 
